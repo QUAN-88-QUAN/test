17b8e2ba7e36e789702aebabc82863c58b053100
QUAN-88-QUAN <q2395816280@gmail.com>
2024-05-31 15:24:49 +0800

    Update index.vue

--- index.vue
+++ index.vue
@@ -37,7 +37,6 @@
         <vxe-table-column :field="`weekDataList.${item}.lastWeekSales`" title="上周销量" min-width="100" />
         <vxe-table-column :field="`weekDataList.${item}.lastWeekSales`" title="上周销量" min-width="100" />
         <vxe-table-column :field="`weekDataList.${item}.lastWeekSales`" title="上周销量" min-width="100" />
-        <vxe-table-column :field="`weekDataList.${item}.lastWeekSales`" title="上周销量" min-width="100" />
         <vxe-table-column :field="`weekDataList.${item}.fbaAvailableReserved`" title="FBA available+ reserved" min-width="100" />
       </vxe-colgroup>
     </vxe-table>
